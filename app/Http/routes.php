9e2e7f809e54808297e3c9df555c90a29354c950
LordMVP <fredyvega43@gmail.com>
2017-12-12 09:45:13 -0500

    Errores

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -64,3 +64,8 @@ Route::group(['prefix' => 'admin'], function()
 {
 		Route::resource('ejercicios', 'ejercicios_controller');
 });
+
+Route::get('error', [
+	'uses'	=>	'errores_controller@index',
+	'as'	=>	'error.pagina'
+	]);
--- resources/views/pagina/error.blade.php
+++ resources/views/pagina/error.blade.php
@@ -1,20 +1,28 @@
 @extends('pagina.principal.principal')
 
-@section('titulo', 'Funciones')
-
+@section('titulo', 'Acceso Denegado')
 
 @section('contenido')
 
-<div class="blog">
-	<div class="container">
-		<div class="blog-text">
-			<h1>401</h1>
-			<p>OPPS No tiene Los Permisos Para Acceder A esta Pagina</p> 
-			<a href="/" >Regresar</a>
-			<span> </span>
+	@include('pagina.funcionalidad.nav')
+<br><br><br>
+<section id="contact">
+  	<div class="container">
+        <h3 class="text-center text-uppercase text-secondary mb-0">Acceso Denegado</h3>
+        <hr class="star-dark mb-5">
+		<div id="page-wrapper">
+			
+			<div class="row">
+				<div class="col-lg-8 mx-auto">
+						<h1>401</h1>
+						<p>OPPS No tiene Los Permisos Para Acceder A esta Pagina</p> 
+						<a href="{{ route('index') }}" >Regresar</a>
+						<span> </span>
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
+</section>
 
 @endsection
 
